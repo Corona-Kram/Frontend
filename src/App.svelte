27de6cc3b8a5488f839c416d0facd8d3bfa5c6ce
Kristin <krik@itu.dk>
2020-04-03 19:27:34 +0200

    made tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
 <script>
-	export let name;
+	import { Tabs, TabList, TabPanel, Tab } from './tabs.js';
+	let count = 0;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+
+	<Tabs>
+		<TabList>
+			<Tab>one</Tab>
+			<Tab>two</Tab>
+			<Tab>three</Tab>
+		</TabList>
+	
+		<TabPanel>
+			<h2>First panel</h2>
+			<main>
+				<button on:click="{() => count += 1}">SEND KRAM</button>
+			</main>
+		</TabPanel>
+	
+		<TabPanel>
+			<h2>Second panel</h2>
+		</TabPanel>
+	
+		<TabPanel>
+			<h2>Third panel</h2>
+		</TabPanel>
+	</Tabs>
+	
+
 
 <style>
 	main {
@@ -13,6 +35,20 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+	button {
+		background-color: white; 
+  		border: 3px solid #555555;
+		border-radius: 8px;
+  		color: black;
+  		padding: 15px 32px;
+  		text-align: center;
+  		text-decoration: none;
+  		display: inline-block;
+		position: absolute;
+    	top: 50%;
+  		font-size: 32px;
+		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
 	}
 
 	h1 {
